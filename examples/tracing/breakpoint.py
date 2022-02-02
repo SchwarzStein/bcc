d61002dd85787db7fe9d438832b34d4a6907e745
SchwarzStein <mpamenas@gmail.com>
2022-02-02 14:24:51 +0100

    fix parser spelling

--- examples/tracing/breakpoint.py
+++ examples/tracing/breakpoint.py
@@ -30,7 +30,7 @@ int func(struct pt_regs *ctx) {
 def get_prog_arguments():
     bp_choices = ['BP_R','BP_W','BP_RW','BP_X']
     bp_values = zip(range(1,4),bp_choices)
-    parser = argprser.ArgumentParser(description="Install breakpoint in process and print hello world when triggered\n")
+    parser = argparse.ArgumentParser(description="Install breakpoint in process and print hello world when triggered\n")
     parser.add_argument('symbol_addr',type=int,help='the address at which the breakpoint will be installed')
     parser.add_argument('pid',type=int,help='the pid of target process')
     parser.add_argument('bp_type',type=str,help='the type of breakpoint to install',choices=bp_choices)
